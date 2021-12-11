87cfcabc6c82c4d9e7032c5fa2cef442f65bda09
Jed Reisner <djechotech@gmail.com>
2021-12-10 23:05:36 -0700

    mouseout

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,13 +1,15 @@
-function loadPopup(event){
-    if($("#popup-overlay").hasClass("popup-hide")){
-        $("#popup-overlay").removeClass("popup-hide");
-    }else{
-        $("#popup-overlay").addClass("popup-show");
+$(body).mouseout(function(event){
+    function loadPopup(event){
+          if($("#popup-overlay").hasClass("popup-hide")){
+              $("#popup-overlay").removeClass("popup-hide");
+          }else{
+              $("#popup-overlay").addClass("popup-show");
+          }
     }
-}
-setTimeout(loadPopup, 3);
-  
-$("#close").click(function(e){
-    $('#popup-overlay').addClass("popup-hide");
-    e.preventDefault();
-})
+     setTimeout(loadPopup, 3);
+        
+      $("#close").click(function(e){
+          $('#popup-overlay').addClass("popup-hide");
+          e.preventDefault();
+      })
+  });
