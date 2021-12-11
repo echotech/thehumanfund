4346d1c2f2201041ff78125d39b10e79ab017b03
Jed Reisner <djechotech@gmail.com>
2021-12-10 22:58:31 -0700

    better popup

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,15 +1,13 @@
-$(document).ready(function(event){
-    function loadPopup(event){
-          if($("#popup-overlay").hasClass("popup-hide")){
-              $("#popup-overlay").removeClass("popup-hide");
-          }else{
-              $("#popup-overlay").addClass("popup-show");
-          }
+function loadPopup(event){
+    if($("#popup-overlay").hasClass("popup-hide")){
+        $("#popup-overlay").removeClass("popup-hide");
+    }else{
+        $("#popup-overlay").addClass("popup-show");
     }
-     setTimeout(loadPopup, 3);
-        
-      $("#close").click(function(e){
-          $('#popup-overlay').addClass("popup-hide");
-          e.preventDefault();
-      })
-  });
+}
+setTimeout(loadPopup, 3);
+  
+$("#close").click(function(e){
+    $('#popup-overlay').addClass("popup-hide");
+    e.preventDefault();
+})
